0ee99a0daafe290e698ce4425246b6a1804d5c8c
Stas Khirman <stas@khirman.com>
2022-10-23 01:40:01 +0300

    support s3:// for WDL input URLs

--- WDL/Tree.py
+++ WDL/Tree.py
@@ -1467,6 +1467,9 @@ async def resolve_file_import(uri: str, path: List[str], importer: Optional[Docu
     if uri.startswith("http://") or uri.startswith("https://"):
         # for now we do nothing with web URIs
         return uri
+    if uri.startswith("s3://") :
+        # for now we do nothing with S3 URIs
+        return uri
     if uri.startswith("file:///"):
         uri = uri[7:]
     if os.path.isabs(uri):
@@ -1497,10 +1500,49 @@ async def read_source_default(
     uri: str, path: List[str], importer: Optional[Document]
 ) -> ReadSourceResult:
     abspath = await resolve_file_import(uri, path, importer)
+    # handle S3path
+    if abspath.startswith("s3://"):
+        return read_source_s3(abspath)
     # TODO: actual async read
     with open(abspath, "r") as infile:
         return ReadSourceResult(source_text=infile.read(), abspath=abspath)
 
+ #####----------------------------------------   
+import boto3
+
+def read_source_s3(
+    abspath: str
+) -> ReadSourceResult:
+    # TODO: actual async read
+    bucket_name, key_name = split_s3_bucket_key(abspath)
+    s3 = boto3.resource('s3')
+    obj = s3.Object(bucket_name, key_name)
+    body = obj.get()['Body'].read().decode('utf-8')
+    return ReadSourceResult(source_text=body, abspath=abspath)
+# from awscli.customizations.s3.utils import split_s3_bucket_key
+def find_bucket_key(s3_path):
+    """
+    This is a helper function that given an s3 path such that the path is of
+    the form: bucket/key
+    It will return the bucket and the key represented by the s3 path
+    """
+    s3_components = s3_path.split('/')
+    bucket = s3_components[0]
+    s3_key = ""
+    if len(s3_components) > 1:
+        s3_key = '/'.join(s3_components[1:])
+    return bucket, s3_key
+
+
+def split_s3_bucket_key(s3_path):
+    """Split s3 path into bucket and key prefix.
+    This will also handle the s3:// prefix.
+    :return: Tuple of ('bucketname', 'keyname')
+    """
+    if s3_path.startswith('s3://'):
+        s3_path = s3_path[5:]
+    return find_bucket_key(s3_path)
+ #####----------------------------------------   
 
 async def _load_async(
     uri: str,
